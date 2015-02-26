4a17e3385c23b9d838df3f4396191ea096ed5485
NL33 <stkellman@gmail.com>
2015-02-26 10:08:22 -0500

    add length limit on tweets, update alert positioning

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
-
+ validates :text, presence: true, length: { maximum: 140, message: "was interesting, but too long. Please try again." }
  belongs_to :user
 
  def show_user_email
