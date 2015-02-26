5feb3bc5ca278de42a3c1c2ec52240d3878be532
NL33 <stkellman@gmail.com>
2015-02-26 10:46:00 -0500

    add letter_opener gem to view emails in development

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
+  def create
+    @user = User.new(params[:user])
+  end
+
 
   #def show
   #  @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
    after_create :send_welcome_message
 
   def send_welcome_message
-    UserMailer.signup_confirm(self).deliver
+    UserMailer.signup_confirmation(self).deliver
   end
 end
